e0056056a892e3189f14fb3dc0b30564895d802a
Jojo-27 <56597637+Jojo-27@users.noreply.github.com>
2019-11-06 11:35:50 +0100

    Add files via upload

---
+++ Site/ReconnaissanceDeTonalité.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Health Foundation</title>
+    <link rel="stylesheet" media="screen" href="design.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  </head>
+  <body>
+
+    <header class="headerNonConnecte" >
+            <div class = logoPrincipal >
+        <img src="HF4.png" class="logo" alt="Logo de Health Foundation">
+        <h1 id="Titre"><a href="accueil.html">Health Foundation</a></h1>
+        </div>
+            <nav id="menu">
+                <ul>
+                    <li><a href="index.html"> Accueil</a></li>
+              <li><a href="apropos.html">A propos </a></li>
+              <li><a href="connexion.html">Connexion</a></li>
+              <li><a href="inscription.html">Inscription</a></li>
+
+            </ul>
+
+            </nav>
+
+    </header>
+
+
+    <div class ="Tonalité">
+    <span class = "Reconnaissance de tonalité">
+      <h1>Reconnaissance de tonalité</h1>
+    </span>
+    </div>
+
+
+
+
+    <footer class="footerNonConnecte">
+            <div class="menuBas">
+                <a href="cgu.html" target="_blank"> CGU</a>
+                <a href="faq.html"> FAQ/Aide</a>
+                <a href="contact.html"> Contact</a>
+                <div id="connexion"><a href="connexion.html" >Connexion</a></div>
+                <p>©Copyright Health Foundation, tout droits réservés</p>
+            </div>
+      </footer>
+
+  </body>
+</html>
