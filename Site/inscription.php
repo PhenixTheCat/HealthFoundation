d177daeaf4363314e1dda9f6750a78e47adc972b
maebain <56313596+maebain@users.noreply.github.com>
2019-11-08 14:03:09 +0100

    Update inscription.php

--- Site/inscription.php
+++ Site/inscription.php
@@ -74,7 +74,7 @@ if(isset($_POST['inscriptionP1'])) {
             <nav id="menu">
                 <ul>
                     <li><a href="index.php"> Accueil</a></li>
-                    <li><a href="apropos.php">À propos </a></li>
+                    <li><a href="aPropos.php">À propos </a></li>
                     <li><a href="connexion.php">Connexion</a></li>
                     <li><a href="inscription.php">Inscription</a></li>
 
