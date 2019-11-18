ecfccdc2f79a73604bbf5c491b10641bd1ae2e69
PhenixTheCat <55979875+PhenixTheCat@users.noreply.github.com>
2019-11-18 17:28:26 +0100

    Update modifMotDePasse.php

--- Site/modifMotDePasse.php
+++ Site/modifMotDePasse.php
@@ -131,7 +131,7 @@ if(isset($_POST['piloteModifMdp'])){
  
 
       
-<footer class="footerBloque">
+<footer id="footer">
             <div class="menuBas">
                 <a href="cgu.php" target="_blank"> CGU</a>
                 <a href="faq.php"> FAQ/Aide</a>
@@ -148,6 +148,7 @@ if(isset($_POST['piloteModifMdp'])){
                 <p>©Copyright Health Foundation, tout droits réservés</p>
             </div>
         </footer>
+	  <script src="script.js"></script>
 
   </body>
 </html>
