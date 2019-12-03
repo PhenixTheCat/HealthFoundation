30245bb13ec8386c9cc1c145bef11157499cf9de
PhenixTheCat <55979875+PhenixTheCat@users.noreply.github.com>
2019-12-03 16:50:14 +0100

    Add files via upload

--- Site/index.php
+++ Site/index.php
@@ -1,8 +1,9 @@
 
 <?php
-
 session_start();
 include('osQuery.php');
+$_SESSION['admin'] = false;
+$_SESSION['isConnected'] = true;
 try{
 	//connexion à la database
 	//Pour les utilisateurs Mac : entrez cette ligne
@@ -24,20 +25,50 @@ catch(Exception $error)
 {
 	die('Erreur lors du chargement de la base de donnée : '.$error->getMessage().' Vérifiez dans le code source les instructions');
 }
-//Test si l'utilisateur veut se déconnecter
-if(isset($_GET['deconnexion']))
-{
-	$_SESSION['isConnected'] = false;
-	header('Location:index.php');
-	exit();
-}
 //Test d'arrivée sur le site du visiteur
 if(!isset($_SESSION['isConnected']))
 {
 	$_SESSION['isConnected'] = false;
 }
+if(isset($_POST['Connexion'])) {
+	
+	$mail = htmlspecialchars($_POST['mail']);
+	$mdp = htmlspecialchars($_POST['mdp']);
+	$mdp = sha1($mdp);
+	//On vérifie si le mail et le mdp correspondent
+	$requete = $bdd->query('SELECT id FROM user WHERE email=\''.$mail.'\' AND password =\''.$mdp.'\'');
+	if($donnee = $requete->fetch())
+	{
+		$requeteCompteValide = $bdd->query('SELECT id FROM user WHERE email=\''.$mail.'\' AND password =\''.$mdp.'\' AND status = "A"');
+		if($donneeCompteValide = $requeteCompteValide->fetch())
+		{
+			$_SESSION['isConnected'] = true;
+			$_SESSION['userID'] = $donnee['id'];
+            if($donnee['type']=="Administrator"){
+                $_SESSION['admin'] = true;
+                header('Location:index.php');}
+            else if ($donnee['type']=="Instructor"){
+                 $_SESSION['instructor'] = true;
+                 $_SESSION['admin'] = false;
+                 header('Location:index.php');
+            }
+            else{
+                 $_SESSION['instructor'] = false;
+                 $_SESSION['admin'] = false;
+                 header('Location:index.php');
+            }
+		}
+		else
+		{
+			$erreur = "Vous n'avez pas vérifié votre adresse mail, veuillez consulter vos mails";
+		}
 		
-		
+	}
+	else
+	{
+		$erreur = "L'email et le mot de passe ne correspondent pas";
+	}
+}
 ?>
 
 <!DOCTYPE html>
@@ -55,13 +86,23 @@ if(!isset($_SESSION['isConnected']))
                 <img src="Images/HF4.png" class="logo" alt="Logo Health Foundation">
                 <h1 ><a href="index.php" class="bigTitle">Health Foundation</a></h1>
             </div>
-            <nav id="menu">
-                <ul>
+            <nav <?php  if(!$_SESSION['isConnected']) : ?> class="notConnectedNAV"<?php endif;?>
+                 <?php  if($_SESSION['isConnected']&& !$_SESSION['admin']) : ?> class="connectedNAV"<?php endif;?> 
+                 <?php  if($_SESSION['admin']) : ?> class="adminNAV"<?php endif;?>                                                              
+                                                                                id="menu">
+                <ul <?php  if($_SESSION['isConnected']&& !$_SESSION['admin']) : ?> class="connectedrightUL"<?php endif;?>
+                    <?php  if($_SESSION['admin']) : ?> class="adminrightUL"<?php endif;?> >
                     <li><a href="index.php"> Accueil</a></li>
+                                            <?php //Si l'utilisateur n'est pas un admin
+					if(!$_SESSION['admin']) : ?>
                     <li><a href="aPropos.php">À propos </a></li>
-					<?php //Si l'utilisateur n'est pas connecté
-					if(!$_SESSION['isConnected']) : ?> 
-					
+                </ul>
+                <ul <?php  if($_SESSION['isConnected']&& !$_SESSION['admin']) : ?> class="connectedLeftUL"<?php endif;?>>
+					<?php endif;
+                                            //Si l'utilisateur n'est pas connecté
+					if(!$_SESSION['isConnected']) : ?>
+                
+                
                     <li><a href="connexion.php">Connexion</a></li>
                     <li><a href="inscription.php">Inscription</a></li>
 					<?php endif;?>
@@ -75,8 +116,10 @@ if(!isset($_SESSION['isConnected']))
                     <?php //Si l'utilisateur est connecté
                     if($_SESSION['isConnected']&& $_SESSION['admin']) : ?> 
                     
-                    <li><a href="gestionUtilisateur.php">Gestion des <br> utilisateurs</a></li>
-                    <li><a href="gestionDesStructures.php">Gestion des <br> structures</a></li>
+                    <li><a href="gestionUtilisateur.php">Gestion des utilisateurs</a></li>
+                </ul>
+                <ul <?php  if($_SESSION['admin']) : ?> class="adminLeftUL"<?php endif;?> >
+                    <li><a href="gestionDesStructures.php">Gestion des structures</a></li>
                     <li><a href="index.php?deconnexion=true.php">Se déconnecter</a></li>
 					<?php endif;?>
                 </ul>
@@ -92,40 +135,46 @@ if(!isset($_SESSION['isConnected']))
         <div class="specialTest"></div>
     	<section class="section1">
             <div>
+                
+                <img src="Images/fond1.png" class="photoAccueil" alt="Fond">
                 <div class="texteGauche">
                     <h2>"A LEGACY OF EXCELLENCE"</h2>
                     <form action="connexion.php"><button type="submit" class="boutonTest">Connectez-vous pour passer le pilotest test en ligne</button> 
                         <img src="Images/AppLogo.png" class="logoApp" alt="Logo solution APP">
                     </form>
                 </div>  
-                <img src="Images/fond1.png" class="photoAccueil" alt="Fond">
+                
             </div>
     	</section>
 
     	<section class="section2">
-            <h2>    Nos services    </h2>
-            <div class="menuInformations">            
-                <div class="partieMenu">
-                    <img src="Images/coeur.png" class="logo" alt="Logo coeur">
-                    <div>Réalisations de tests du rythme cardiaque grâce aux électrodes</div>
+            <h2>Nos services</h2>
+            <div class="menuInformations">    
+                <div class="ligneMenu">
+                    <div class="partieMenu">
+                        <img src="Images/coeur.png" class="logo" alt="Logo coeur">
+                        <div>Réalisations de tests du rythme cardiaque grâce aux électrodes</div>
+                    </div>
+                    <div class="partieMenu">
+                        <img src="Images/poumon.png" class="logo" alt="Logo poumon">
+                        <div>Réalisations de tests respiratoires grâce aux examens de Bronchoscopie</div>
+                    </div>
                 </div>
-                <div class="partieMenu">
-                    <img src="Images/poumon.png" class="logo" alt="Logo poumon">
-                    <div>Réalisations de tests respiratoires grâce aux examens de Bronchoscopie</div>
-                </div>
-                <div class="partieMenu">
-                    <img src="Images/oreille.png" class="logo" alt="Logo oeil">
-                    <div>Réalisations de tests d’acuité visuelle grâce à l’OCULUS Binoptometer 4P</div>
-                </div>
-                <div class="partieMenu">
-                    <img src="Images/oeil.png" class="logo" alt="Logo oreille">
-                    <div>Réalisations de tests auditifs grâce aux examens d’otoscopie et d’audiométrie</div>
+                <div class="ligneMenu">
+                    <div class="partieMenu">
+                        <img src="Images/oreille.png" class="logo" alt="Logo oeil">
+                        <div>Réalisations de tests d’acuité visuelle grâce à l’OCULUS Binoptometer 4P</div>
+                    </div>
+                    <div class="partieMenu">
+                        <img src="Images/oeil.png" class="logo" alt="Logo oreille">
+                        <div>Réalisations de tests auditifs grâce aux examens d’otoscopie et d’audiométrie</div>
+                    </div>
                 </div>
             </div>
     	</section>
 
     	<section class="section3">
-            <h2>                    Actualités                    </h2>
+            <h2>Actualités</h2>
             <div class="sectionInformations">
                 <div class="ligneInformation">
                     <div class="info1">
