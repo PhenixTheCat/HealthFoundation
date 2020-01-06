7d61be02e9a4b94c23958b716de71b31bed32f28
Chen Eva <eva.chen@isep.fr>
2020-01-06 14:24:59 +0100

    changement graphiques

--- Site/controleurs/user.php
+++ Site/controleurs/user.php
@@ -515,15 +515,15 @@ switch ($function) {
     case 'chartTonality':
         $vue = "chart";
         $error = false;
-        if(getTestTimeline($database,$_SESSION['pilotId'],3)!=array(null)&&getTestValue($database,$_SESSION['pilotId'],3)!=array(null)){
-        drawPointGraphics("Resultats de la reconnaissance de tonalité en fonction du temps",(getTestTimeline($database,$_SESSION['pilotId'],3)),(getTestValue($database,$_SESSION['pilotId'],3)));
+        if(getTestTimeline($database,$_SESSION['pilotId'],4)!=array(null)&&getTestValue($database,$_SESSION['pilotId'],4)!=array(null)){
+        drawPointGraphics("Resultats de la reconnaissance de tonalité en fonction du temps",(getTestTimeline($database,$_SESSION['pilotId'],4)),(getTestValue($database,$_SESSION['pilotId'],4)));
         }
         break;
 
     case 'chartReactionTime':
         $vue = "chart";
         $error = false;
-        if(getTestTimeline($database,$_SESSION['pilotId'],1)!=array(null)&&getTestValue($database,$_SESSION['pilotId'],2)!=array(null)){
+        if(getTestTimeline($database,$_SESSION['pilotId'],2)!=array(null)&&getTestValue($database,$_SESSION['pilotId'],2)!=array(null)){
         drawPointGraphics("Resultats du temps de réaction en fonction du temps",(getTestTimeline($database,$_SESSION['pilotId'],2)),(getTestValue($database,$_SESSION['pilotId'],2)));
         }
       
@@ -533,8 +533,8 @@ switch ($function) {
     case 'chartPerception':
         $vue = "chart";
         $error = false;
-        if(getTestTimeline($database,$_SESSION['pilotId'],1)!=array(null)&&getTestValue($database,$_SESSION['pilotId'],4)!=array(null)){
-        drawPointGraphics("Resultats du seuil de perception en fonction du temps",(getTestTimeline($database,$_SESSION['pilotId'],4)),(getTestValue($database,$_SESSION['pilotId'],4)));
+        if(getTestTimeline($database,$_SESSION['pilotId'],3)!=array(null)&&getTestValue($database,$_SESSION['pilotId'],3)!=array(null)){
+        drawPointGraphics("Resultats du seuil de perception en fonction du temps",(getTestTimeline($database,$_SESSION['pilotId'],3)),(getTestValue($database,$_SESSION['pilotId'],3)));
         }
 
         break;
