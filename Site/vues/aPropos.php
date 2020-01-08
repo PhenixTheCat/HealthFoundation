c3ff5c29eedb2c3c86c2c4446b7fc8e76e1f2b23
Jojo-27 <56597637+Jojo-27@users.noreply.github.com>
2020-01-08 10:39:15 +0100

    Update aPropos.php

--- Site/vues/aPropos.php
+++ Site/vues/aPropos.php
@@ -36,27 +36,22 @@
     <p>Nos investisseurs sont aujourd’hui : l'État, à travers les aides financières fournies par le Ministère des
       Affaires Sociales et de la Santé, et l'ISEP, qui nous fournit le matériel et nous accompagne pendant la mise en
       place de notre projet. </p>
-    <h3> Notre produit : </h3>
+  </div>
+  
+   <div class="titreProduit">
+        <h3> Notre produit : </h3>
   </div>
 
   <div class="produit">
     <span class="produit">
-      <h4>Description du produit </br></br>Nos capteurs de dernière technologie, munis de 5 électrocardiogrammes (ECG)
-        permettent d'enregistrer l'activité cardiaque du patient.
-        En effet, ces capteurs sont basés sur la mesure des courants électriques qui traversent l'organe à chacune de
-        ses contractions. Le produit
-        se présente sous la forme de 4 électrodes (ou capteurs) qui sont placées sur la peau (devant être totalement
-        découverte et dépourvue de pilosité)
-        du patient et directement reliées à l'appareil d'enregistrement. Par la suite, les électrodes vont pouvoir
-        capter des champs électriques produits
-        par le coeur et décrire son activité en temps réel.</br></br>
-        D'autre part, nos capteurs sont équipés de mini capteurs à microfluides permettant de relever et d'analyser la
-        transpiration du patient. La mise au
-        point de ce tout nouveau type de capteur permet aujourd'hui aux équipes médicales de contrôler l'état d'un
-        patient afin de déceler des maladies cardiovasculaires,
-        des diabètes ou d'autre pathologies de ce genre, mais également de mesurer l'état de stress et d'anxitété d'une
-        personne.
-      </h4>
+     <p>Description du produit </br></br>Nos capteurs de dernière technologie, munis d'un électrocardiogramme (ECG) permettent d'enregistrer l'activité cardiaque du patient.
+        En effet, ces capteurs sont basés sur la mesure des courants électriques qui traversent l'organe à chacune de ses contractions. Le produit
+        se présente sous la forme d'une pince électrode (ou capteur) qui est placées sur le doigt du patient et directement reliées à l'appareil d'enregistrement. 
+        Par la suite, l'électrode va pouvoir capter des champs électriques produits par le coeur et décrire son activité en temps réel.</br></br>
+        D'autre part, nos capteurs sont équipés de mini capteurs à microfluides permettant de relever et d'analyser la transpiration du patient. La mise au 
+        point de ce tout nouveau type de capteur permet aujourd'hui aux équipes médicales de contrôler l'état d'un patient afin de déceler des maladies cardiovasculaires,
+        des diabètes ou d'autre pathologies de ce genre, mais également de mesurer l'état de stress et d'anxitété d'une personne.
+        </p>
     </span>
   </div>
 
