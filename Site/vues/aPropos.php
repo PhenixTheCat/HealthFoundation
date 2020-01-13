fda224c429616fa2a782feebc825ed8c0afb6c2b
PhenixTheCat <55979875+PhenixTheCat@users.noreply.github.com>
2020-01-13 16:04:11 +0100

    Add files via upload

--- Site/vues/aPropos.php
+++ Site/vues/aPropos.php
@@ -15,7 +15,7 @@
 
 
   <div class="headerContact">
-    <h1> À propos </h1>
+    <h1> A propos </h1>
   </div>
 
   <div class="about">
@@ -28,42 +28,41 @@
       aux besoins des cabinets médicaux, des hôpitaux, des compagnies aériennes et militaires. Aujourd'hui leader
       européen dans la production de matériel "High-Tech" médical, Health Foundation réalise certaines exportations à
       l'étranger pour répondre aux demandes des entreprises internationales. </p>
-    <h3> Notre credo : </h3>
+    <h3> Notre credo</h3>
     <p>“A Legacy of Excellence” Lors de sa création, ses co-fondateurs sortaient tout juste de l'ISEP, nommément CHEN
       Eva, responsable technique, DALUZ Mattéo, chef de projet, ESTRIBEAU Elias, coordinateur, NORA Ariel, responsable
       communication, PHAM Jonathan, designer technique et moi-même, BAIN Maë, directrice générale.</p>
-    <h3> Nos investisseurs </h3>
+    <h3> Nos investisseurs</h3>
     <p>Nos investisseurs sont aujourd’hui : l'État, à travers les aides financières fournies par le Ministère des
       Affaires Sociales et de la Santé, et l'ISEP, qui nous fournit le matériel et nous accompagne pendant la mise en
       place de notre projet. </p>
   </div>
   
-   <div class="titreProduit">
-        <h3> Notre produit : </h3>
-  </div>
+    <div class="encadreProduit">
+    
+        <div class="titreProduit">
+            <h3> Notre produit</h3>
+        </div>
+        
+        <img src="https://raw.githubusercontent.com/PhenixTheCat/HealthFoundation/master/Site/Images/capteur2.jpg" class="capteur" alt="Photo capteurs">
+        
+        <div class="produit">
+            <p>Nos capteurs de dernière technologie, munis d'un électrocardiogramme (ECG) permettent d'enregistrer l'activité cardiaque du patient.
+               En effet, ces capteurs sont basés sur la mesure des courants électriques qui traversent l'organe à chacune de ses contractions. Le produit
+               se présente sous la forme d'une pince électrode (ou capteur) qui est placées sur le doigt du patient et directement reliées à l'appareil d'enregistrement. 
+               Par la suite, l'électrode va pouvoir capter des champs électriques produits par le coeur et décrire son activité en temps réel.</br></br>
+               D'autre part, nos capteurs sont équipés de mini capteurs à microfluides permettant de relever et d'analyser la transpiration du patient. La mise au 
+               point de ce tout nouveau type de capteur permet aujourd'hui aux équipes médicales de contrôler l'état d'un patient afin de déceler des maladies cardiovasculaires,
+               des diabètes ou d'autre pathologies de ce genre, mais également de mesurer l'état de stress et d'anxitété d'une personne.
+               </p>
+        </div>
+        
+        
 
-  <div class="produit">
-    <span class="produit">
-     <p>Description du produit </br></br>Nos capteurs de dernière technologie, munis d'un électrocardiogramme (ECG) permettent d'enregistrer l'activité cardiaque du patient.
-        En effet, ces capteurs sont basés sur la mesure des courants électriques qui traversent l'organe à chacune de ses contractions. Le produit
-        se présente sous la forme d'une pince électrode (ou capteur) qui est placées sur le doigt du patient et directement reliées à l'appareil d'enregistrement. 
-        Par la suite, l'électrode va pouvoir capter des champs électriques produits par le coeur et décrire son activité en temps réel.</br></br>
-        D'autre part, nos capteurs sont équipés de mini capteurs à microfluides permettant de relever et d'analyser la transpiration du patient. La mise au 
-        point de ce tout nouveau type de capteur permet aujourd'hui aux équipes médicales de contrôler l'état d'un patient afin de déceler des maladies cardiovasculaires,
-        des diabètes ou d'autre pathologies de ce genre, mais également de mesurer l'état de stress et d'anxitété d'une personne.
-        </p>
-    </span>
-  </div>
-
-  <div class="imageProduit">
-    <span class="Image produit">
-      <img src="Images/capteur2.jpg" class="capteur" alt="Photo capteurs">
-    </span>
-  </div>
+    </div>
 
 
-
-  <script src="script.js"></script>
+  
 
 
 </body>
