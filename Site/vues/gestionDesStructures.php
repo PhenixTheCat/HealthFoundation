87a696a698f9268270b4ebb855bd486c9cee7f03
Chen Eva <eva.chen@isep.fr>
2020-01-15 10:35:20 +0100

    correction boutons

--- Site/vues/gestionDesStructures.php
+++ Site/vues/gestionDesStructures.php
@@ -169,7 +169,7 @@
 		<script>showCriterias(0,1,true,0);</script>
 
 		<div id="Criterias" class = "Criterias"><b></b></div>
-		<button type = "submit" value = "Research" name="Research">Rechercher</button>
+		<button class="submitButtons" type = "submit" value = "Research" name="Research">Rechercher</button>
 	</form>
 
 
